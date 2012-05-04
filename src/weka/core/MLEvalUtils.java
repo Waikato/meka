20b2ea62c82fa6841b5c1b247b9e6b4be4fe6296
jread82_nz <jread82_nz@99739a35-adc7-4a81-af46-22eadfc39334>
2012-05-04 13:32:36 +0000

    new

---
+++ src/weka/core/MLEvalUtils.java
@@ -0,0 +1,306 @@
+package weka.core;
+
+import weka.core.*;
+import java.util.*;
+
+public abstract class MLEvalUtils {
+
+	public static String getThreshold(ArrayList<double[]> Y, Instances D, String top) throws Exception {
+		if (top.equals("c")) {			// Calbirate threshold
+			return String.valueOf(MLEvalUtils.calibrateThreshold(Y,MLUtils.labelCardinality(D)));
+		}	
+		else if (top.equals("C")) {		// Calibrate thresholds (one for each label)
+			return Arrays.toString(MLEvalUtils.calibrateThresholds(Y,MLUtils.labelCardinalities(D)));
+		}
+		else { 							// Set our own threshold
+			return String.valueOf(Double.parseDouble(top));
+		}
+	}
+
+	/**
+	 * Calibrate a threshold.
+	 * Based upon a supplied label cardinality from the training set.
+	 */
+	public static double calibrateThreshold(ArrayList<double[]> Y, double LC_train) { 
+		int N = Y.size();
+		ArrayList<Double> big = new ArrayList<Double>();
+		for(double y[] : Y) {
+			for (double y_ : y) {
+				big.add(y_);
+			}
+		}
+		Collections.sort(big);
+
+		System.out.println("we will take = "+(int)Math.round(LC_train * (double)N));
+		int i = big.size() - (int)Math.round(LC_train * (double)N);
+
+		System.out.println("from "+i);
+		if (N == big.size()) { // special cases
+			if (i+1 == N) // only one!
+				return (big.get(N-2)+big.get(N-1)/2.0);
+			if (i+1 >= N) // zero!
+				return 1.0;
+			else
+				return Math.max(((double)(big.get(i)+big.get(i+1))/2.0), 0.00001);
+		}
+		/*
+		//System.out.println(""+big);
+		//System.out.println("N = "+N);
+		//System.out.println("big.size = "+big.size());
+		*/
+		System.out.println("LCard ("+LC_train+") between "+i+" and "+(Math.max(i+1,N-1)));
+		//if (i == big.size) // we have to set the threshold very low
+		//	return 0.00001
+		return Math.max(((double)(big.get(i)+big.get(Math.max(i+1,N-1))))/2.0 , 0.00001);
+	}
+
+	/**
+	 * Calibrate the threshold[] vector.
+	 * Based upon a supplied label cardinality from the training set.
+	 * Assumes that each double[] is of length L;
+	 */
+	public static double[] calibrateThresholds(ArrayList<double[]> Y, double LC_train[]) { 
+
+		int L = LC_train.length;
+		double t[] = new double[L];
+
+		ArrayList<double[]> Y_[] = new ArrayList[Y.size()];
+		for(int j = 0; j < L; j++) {
+			Y_[j] = new ArrayList<double[]>();
+		}
+
+		for(double y[] : Y) {
+			for(int j = 0; j < L; j++) {
+				Y_[j].add(new double[]{y[j]});
+			}
+		}
+
+		for(int j = 0; j < L; j++) {
+			t[j] = calibrateThreshold(Y_[j],LC_train[j]);
+		}
+
+		return t;
+	}
+
+	public static HashMap<String,Double> getMLStats(ArrayList<double[]> Rankings, ArrayList<int[]> Actuals, String t) {
+		double ts[] = null;
+		if (t.startsWith("[")) {
+			ts = MLUtils.toDoubleArray(t);							// threshold vector       [t1 t2 ... tL]]
+		}
+		else {
+			ts = new double[Rankings.iterator().next().length];
+			Arrays.fill(ts,Double.parseDouble(t));					// make a threshold vector [t t t ... t]
+		}
+		return getMLStats(Rankings,Actuals,ts);
+	}
+
+	/**
+	 * Calculate Performance Measures. 
+	 * A threshold is supplied. Rankings.size() must equal Actuals.size(), and they must correspond.
+	 */
+	public static HashMap<String,Double> getMLStats(ArrayList<double[]> Rankings, ArrayList<int[]> Actuals, double t[]) {
+
+		double N = Rankings.size();
+		int L = Rankings.iterator().next().length;
+		int fp = 0, tp = 0, tn = 0, fn = 0;
+		int p_sum_total = 0, r_sum_total = 0;
+		double log_loss_D = 0.0, log_loss_L = 0.0;
+		int set_empty_total = 0, set_inter_total = 0; 
+		int exact_match = 0, one_error = 0, coverage = 0; 
+		double accuracy = 0.0, f1_macro_D = 0.0, f1_macro_L = 0.0;
+		int hloss_total = 0;
+		int[] o_tp = new int[L], o_fp = new int[L], o_fn = new int[L], o_tn = new int[L];
+		int[] d_tp = new int[(int)N], d_fp = new int[(int)N], d_fn = new int[(int)N], d_tn = new int[(int)N];
+		//double average_accuracy_online = 0.0;
+
+
+		for(int i = 0; i < N; i++) {
+			double ranking[] = Rankings.get(i);
+			int actual[] = Actuals.get(i);
+
+			int pred[] = new int[actual.length];
+			for(int j = 0; j < L; j++) {
+				pred[j] = (ranking[j] >= t[j]) ? 1 : 0;
+			}
+
+			//System.out.println("act"+Arrays.toString(actual));
+			//System.out.println("prd"+Arrays.toString(pred));
+
+			// calculate
+			int p_sum = 0, r_sum = 0;
+			int set_union = 0;
+			int set_inter = 0;
+			int doc_inter = 0;
+			int doc_union = 0;
+			for(int j = 0; j < L; j++) {
+				int p = pred[j];
+				int R = actual[j];
+				if (p==1) {
+					p_sum++;
+					// predt 1, real 1
+					if(R==1) {
+						r_sum++;
+						tp++;
+						o_tp[j]++;			// f1 macro (L)
+						d_tp[i]++;			// f1 macro (D)
+						set_inter++;
+						set_union++;
+					}
+					// predt 1, real 0
+					else {
+						fp++;
+						o_fp[j]++;			// f1 macro (L)
+						d_fp[i]++;			// f1 macro (D)
+						hloss_total++;
+						set_union++;
+					}
+				}
+				else {                      
+					// predt 0, real 1
+					if(R==1) { 
+						r_sum++;
+						fn++;
+						o_fn[j]++;			// f1 macro (L)
+						d_fn[i]++;			// f1 macro (D)
+						hloss_total++;
+						set_union++;
+					}
+					// predt 0, real 0
+					else {   
+						tn++;
+						o_tn[j]++;			// f1 macro (L)
+						d_tn[i]++;			// f1 macro (D)
+					}
+				}
+
+				// log losses: 
+				log_loss_D += calcLogLoss((double)R,ranking[j],Math.log(N));
+				log_loss_L += calcLogLoss((double)R,ranking[j],Math.log(L));
+			}
+
+			set_inter_total += set_inter;
+
+			p_sum_total += p_sum; 
+			r_sum_total += r_sum;
+
+			if(set_union > 0)	//avoid NaN
+				accuracy += ((double)set_inter / (double)set_union);
+			//System.out.println(""+set_inter+","+set_union);
+
+			if (p_sum <= 0)	//empty set
+				set_empty_total ++;
+
+			// exact match (eval. by example)
+			if(set_inter == set_union)
+				exact_match++;
+
+			// f1 macro average by example
+			if (p_sum > 0 && r_sum > 0 && set_inter > 0) {
+				double prec = (double)set_inter / (double)p_sum;
+				double rec = (double)set_inter / (double)r_sum;
+				if (prec > 0 || rec > 0) {
+					f1_macro_D += ((2.0 * prec * rec) / (prec + rec));
+				}
+			}
+
+			//one error: how many times the top ranked label is NOT in the label set
+			if(actual[Utils.maxIndex(ranking)] <= 0)
+				one_error++;
+
+		}
+
+		double fms[] = new double[L];                                                                                                                        
+		for (int j = 0; j < L; j++) {                                                                                                                        
+			// micro average
+			if(o_tp[j] <= 0)                                                                                             
+				fms[j] = 0.0;                                                                                                                                  
+			else {                                                                                                                                             
+				double prec = (double)o_tp[j] / ((double)o_tp[j]+(double)o_fp[j]);                                                          
+				double recall = (double)o_tp[j] / ((double)o_tp[j]+(double)o_fn[j]);                                                          
+				fms[j] = 2 * ((prec*recall) / (prec+recall));                                                                                                                  
+			}                                                                                                                                                  
+		}               
+
+		double precision = (double)set_inter_total / (double)p_sum_total;
+		double recall = (double)set_inter_total / (double)r_sum_total;
+		
+		HashMap<String,Double> results = new LinkedHashMap<String,Double>();
+		results.put("N"					,N);
+		results.put("L"					,(double)L);
+		results.put("Accuracy"			,(accuracy/N));
+		results.put("H_loss"			,((double)hloss_total/((double)N*(double)L)));
+		results.put("H_acc"				,1.0-((double)hloss_total/((double)N*(double)L)));
+		results.put("Exact_match"		,((double)exact_match/N));
+		results.put("ZeroOne_loss"		,1.0-((double)exact_match/N));
+		//results.put("LCard_diff"		,Math.abs((((double)p_sum_total/N)-(double)r_sum_total/N)));
+		//results.put("Coverage"			,((double)coverage/N));
+		results.put("One_error"			,((double)one_error/N));
+		results.put("LogLossD"			,(log_loss_D/N));
+		results.put("LogLossL"			,(log_loss_L/N));
+		//results.put("EmptyAccuracy"		,(accuracy/(N-set_empty_total)));
+		//results.put("EmptyMacroF1"		,f1_macro_D/(N-(double)set_empty_total));
+		//results.put("Build_time"		,s.vals.get("Build_time"));
+		//results.put("Test_time"		,s.vals.get("Test_time"));
+		//results.put("Total_time"		,s.vals.get("Build_time") + s.vals.get("Test_time"));
+		//results.put("TPR"				,(double)tp / (double)(tp + fn));
+		//results.put("FPR"				,(double)fp / (double)(fp + tn));
+		results.put("Precision"			,precision);
+		results.put("Recall"			,recall);
+		results.put("F1_micro"			,(2.0 * precision * recall) / (precision + recall));
+		results.put("F1_macro_D"		,(f1_macro_D/N));
+		results.put("F1_macro_L"		,(Utils.sum(fms)/(double)L));
+		results.put("EmptyVectors"		,(double)set_empty_total/(double)N);
+		results.put("LCard_pred"		,(double)p_sum_total/N);
+		results.put("LCard_real"		,(double)r_sum_total/N);
+		//results.put("Threshold"        ,t[0]);	// only stores the first threshold
+		//results.put("AUPRC"        		,0.0); // "@see (`Hierarchical Multi-label Classification' by Vens et al, 2008);
+
+		return results;
+
+	}
+
+	public static double calcLogLoss(double R, double P, double C) {
+		// base 2 ?
+		double ans = Math.min(Utils.eq(R,P) ? 0.0 : -( (R * Math.log(P)) + ((1.0 - R) * Math.log(1.0 - P)) ),C);
+		return (Double.isNaN(ans) ? 0.0 : ans);
+	}
+
+	public static HashMap<String,Double> getMTStats(ArrayList<double[]> Rankings, ArrayList<int[]> Actuals) {
+
+		double N = Rankings.size();
+		int L = Actuals.iterator().next().length;
+		double h_loss = 0.0, e_loss = 0.0;
+		for (int i = 0, c = 0; i < N; i++) {
+
+			double y_pred[] = Rankings.get(i);
+			int y_real[] = Actuals.get(i);
+
+			double loss = 0.0;
+			for(int j = 0; j < L; j++) {
+				loss += (y_real[j] != (int)Math.round(y_pred[j])) ? 1.0 : 0.0;
+			}
+			h_loss += loss;
+			e_loss += (loss > 0.0) ? 1.0 : 0.0;
+
+			//System.out.print(" eval : "+MLUtils.toBitString(y_real));
+			//System.out.print(" vs " +MLUtils.toBitString(y_pred));
+			//System.out.println(" : "+loss);
+		}
+
+		HashMap<String,Double> output = new HashMap<String,Double>();
+		output.put("N"					,N);
+		output.put("L"					,(double)L);
+		output.put("L_loss"			,((double)h_loss/((double)N*(double)L)));
+		output.put("L_acc"				,1.0-((double)h_loss/((double)N*(double)L)));
+		output.put("E_loss"			,(e_loss/N));
+		output.put("E_acc"				,1.0-(e_loss/N));
+		return output;
+	}
+
+	public static void main(String args[]) {
+		//for(int i = 0; i < ; i++) {
+		//}
+		//System.out.println(""+Arrays.toString(invert(new int[]{5,2},6)));
+	}
+
+}
--- src/weka/core/MLUtils.java
+++ src/weka/core/MLUtils.java
@@ -14,7 +14,7 @@ public abstract class MLUtils {
 		if(name.indexOf(':') > 0) {
 			return name.substring(name.indexOf(':')+1).split(" ");
 		}
-		else return null;
+		else return new String[]{};
 	}
 
 	//Look for dataset name in the @relation name: in format 'dataset-name: options'
@@ -83,11 +83,19 @@ public abstract class MLUtils {
 		return sb.toString();
 	}
 
-	// may not need this
+	// may not need this // yes we do
 	public static final String toBitString(int i[]) {
 		StringBuilder sb = new StringBuilder(i.length);  
 		for (int b : i) {
 			sb.append(b);
+		}
+		return sb.toString();
+	}
+
+	public static final String toBitString(double d[]) {
+		StringBuilder sb = new StringBuilder(d.length);  
+		for (double b : d) {
+			sb.append((int)Math.round(b));
 		}
 		return sb.toString();
 	}
@@ -101,50 +109,127 @@ public abstract class MLUtils {
 		return d;
 	}
 
-	// Label Cardinality.
-	public static final double labelCardinality(Instances instances) {
-		int sum = 0;
-		for(int i = 0; i < instances.numInstances(); i++) {
-			sum += bitCount(instances.instance(i).stringValue(0));
-		}
-		return (double)sum/(double)instances.numInstances();
+	// "[1,2]" -> [1,2]
+	public static final int[] toIntArray(String s) {
+		s = new String(s.trim());
+		return toIntArray((s.substring(1,s.length()-1)).split(","));
 	}
 
-	// LabelCardinality. Assuming binary format.
-	public static final double labelCardinality(Instances instances, int N) {
+	// raw instance to bit string (i.e. from binary representation)
+	public static final int[] toIntArray(Instance x, int L) {
+		int y[] = new int[L];
+		for(int j = 0; j < L; j++) {
+			y[j] = (int)Math.round(x.value(j));
+		}
+		return y;
+	}
+	// raw instance to bit string (i.e. from binary representation)
+	public static final int[] toIntArray(double w[], double t) {
+		int y[] = new int[w.length];
+		for(int j = 0; j < w.length; j++) {
+			if (w[j] >= t) 
+				y[j] = 1;
+		}
+		return y;
+	}
+
+	public static final int[] toIntArray(String s[]) {
+		int y[] = new int[s.length];
+		for(int j = 0; j < s.length; j++) {
+			y[j] = Integer.parseInt(s[j].trim());
+		}
+		return y;
+	}
+
+	// "[1.0,2.0]" -> [1.0,2.0]
+	public static final double[] toDoubleArray(String s) {
+		s = new String(s.trim());
+		/*
+		if (s.startsWith("[")) {
+			s = s.substring(1);
+		}
+		if (s.endsWith("]")) {
+			s = s.substring(0,s.length()-1);
+		}
+		*/
+		return toDoubleArray((s.substring(1,s.length()-1)).split(","));
+	}
+
+	public static final double[] toDoubleArray(String s[]) {
+		double y[] = new double[s.length];
+		for(int j = 0; j < s.length; j++) {
+			y[j] = Double.parseDouble(s[j].trim());
+		}
+		return y;
+	}
+
+	// Label Cardinality.
+	public static final double labelCardinality(Instances D) {
+		return labelCardinality(D,D.classIndex());
+	}
+
+	// LabelCardinality. 
+	public static final double labelCardinality(Instances D, int L) {
 		double sum = 0.0;
-		for(int i = 0; i < instances.numInstances(); i++) {
-			for(int j = 0; j < N; j++) {
-				sum += instances.instance(i).value(j);
+		for(int i = 0; i < D.numInstances(); i++) {
+			for(int j = 0; j < L; j++) {
+				sum += D.instance(i).value(j);
 			}
 		}
-		return (double)sum/(double)instances.numInstances();
+		return (double)sum/(double)D.numInstances();
+	}
+
+	// Label Cardinalities.
+	public static final double[] labelCardinalities(Instances D) {
+		int L = D.classIndex();
+		double lc[] = new double[L];
+		for(int j = 0; j < L; j++) {
+			for(int i = 0; i < D.numInstances(); i++) {
+				lc[j] += D.instance(i).value(j);
+			}
+			lc[j] /= D.numInstances();
+		}
+		return lc;
 	}
 
 	// Most Common Combination. Assuming binary format.
-	public static final String mostCommonCombination(Instances instances, int N) {
-		HashMap<String,Integer> hm = new HashMap<String,Integer>(instances.numInstances());
+	public static final String mostCommonCombination(Instances D) {
+		return mostCommonCombination(D,D.classIndex());
+	}
+
+	public static final String mostCommonCombination(Instances D, int L) {
+		HashMap<String,Integer> hm = new HashMap<String,Integer>(D.numInstances());
 		double max_v  = 0.0;
 		int max_i = 0;
 
-		for(int i = 0; i < instances.numInstances(); i++) {
-			String comb = MLUtils.toBitString(instances.instance(i),N);
+		for(int i = 0; i < D.numInstances(); i++) {
+			String y = MLUtils.toBitString(D.instance(i),L);
 
-			Integer v = hm.get(comb);
+			Integer v = hm.get(y);
 			if (v == null) {
-				hm.put(comb,0); 
+				hm.put(y,0); 
 			} else {
 				if (v > max_v) {
 					max_v = v;
 					max_i = i;
 				}
-				hm.put(comb,v+1);
+				hm.put(y,v+1);
 			}
-
 		}
 
-		return MLUtils.toBitString(instances.instance(max_i),N);
+		return MLUtils.toBitString(D.instance(max_i),L);
 	}
+
+	// the number of chars different in the two strings
+	public static final int bitDifference(String s1, String s2) {
+		int sum = 0;
+		for(int i = 0; i < s1.length(); i++) {
+			if (s1.charAt(i) != s2.charAt(i))
+				sum++;
+		}
+		return sum;
+	}
+
 
 	// BitCount. Count relevant labels
 	public static final int bitCount(String s) {
@@ -160,13 +245,43 @@ public abstract class MLUtils {
 		return (int)(c - '0');
 	}
 
-	public static final Count countCombinations(Instances instances, int numClasses) {
-		Count distinctCombinations = new Count();  
-		for (int i = 0; i < instances.numInstances(); i++) {
-			String combo = MLUtils.toBitString(instances.instance(i),numClasses);
-			distinctCombinations.add(combo); 
+	public static final HashMap<String,Integer> countCombinations(Instances D, int L) {
+		HashMap<String,Integer> map = new HashMap<String,Integer>();  
+		for (int i = 0; i < D.numInstances(); i++) {
+			String y = MLUtils.toBitString(D.instance(i),L);
+			Integer c = map.get(y);
+			map.put(y,c == null ? 1 : c+1);
 		}
-		return distinctCombinations;
+		return map;
+	}
+
+	// @TODO remove either this or the following function
+	public static final Object maxItem(HashMap<?,Double> map) {
+		Object max_k = null;
+		double max_v = 0.0;
+		for (Object k : map.keySet()) {
+			if (map.get(k) >= max_v) {
+				max_k = k;
+				max_v = map.get(k);
+			}
+		}
+		return max_k;
+	}
+
+	public static final String maxCombination(HashMap<String,Integer> count) {
+
+		int max_v  = 0;
+		String max_s = null;
+
+		for(String s : count.keySet()) {
+			Integer v = count.get(s);
+
+			if (v > max_v) {
+					max_v = v;
+					max_s = s;
+				}
+			}
+		return max_s;
 	}
 
 	/**
@@ -176,14 +291,12 @@ public abstract class MLUtils {
 		System.out.println("Switching attributes: ");
 		int d = D.numAttributes();
 		for(int j = 0; j < L; j++) {
-			System.out.print(".");
 			D.insertAttributeAt(D.attribute(d-1).copy(D.attribute(d-1).name()+"-"),0);
 			for(int i = 0; i < D.numInstances(); i++) {
 				D.instance(i).setValue(0,D.instance(i).value(d));
 			}
 			D.deleteAttributeAt(d);
 		}
-		System.out.println("");
 		return D;
 	}
 
@@ -237,6 +350,15 @@ public abstract class MLUtils {
 		return x;
 	}
 
+	// copy x_dest[j+offset] = x_src[i+from]
+	public static final Instance copyValues(Instance x_dest, Instance x_src, int from, int offset) {
+		int d = x_src.numAttributes();
+		for(int i = from, j = 0; i < d; i++, j++) {
+			x_dest.setValue(j+offset,x_src.value(i));
+		}
+		return x_dest;
+	}
+
 	/* not in use
 	public static int max(int array[]) {
 		int max = Integer.MIN_VALUE;
@@ -247,6 +369,19 @@ public abstract class MLUtils {
 		return max;
 	}
 	*/
+
+	public static int mode(int a[]) {
+		int max = 0;
+		int count = 0;
+		HashMap<Integer,Integer> d = new HashMap<Integer,Integer>();
+		for(int v : a) {
+			int n = d.containsKey(v) ? d.get(v) + 1 : 1;
+			d.put(v,n);
+			if (n > count)
+				max = v;
+		}
+		return max;
+	}
 
 	public static String printAsTextMatrix(double M[][]) {
 		StringBuilder sb = new StringBuilder("M = [\n");  
@@ -261,13 +396,150 @@ public abstract class MLUtils {
 		return sb.toString();
 	}
 
-	public static void main(String args[]) {
-		System.out.println(""+Arrays.toString(invert(new int[]{1,2},6)));
-		System.out.println(""+Arrays.toString(invert(new int[]{0,2},6)));
-		System.out.println(""+Arrays.toString(invert(new int[]{5,2},6)));
+	public static void pruneCountHashMap(HashMap<?,Integer> hm, int p) {
+		ArrayList al = new ArrayList();  
+		for (Object o : hm.keySet()) {
+			if(hm.get(o) <= p) {
+				al.add(o);
+			}
+		}
+		for (Object o : al) {
+			hm.remove(o);
+		}
+		al.clear();
+		al = null;
 	}
 
+	// assume that no hm.get(.) > N
+	public static void pruneCountHashMapBasedAsAFractionOf(HashMap<?,Integer> hm, double p, int N) {
+		ArrayList al = new ArrayList();  
+		for (Object o : hm.keySet()) {
+			if((double)hm.get(o)/(double)N <= p) {
+				al.add(o);
+			}
+		}
+		for (Object o : al) {
+			hm.remove(o);
+		}
+		al.clear();
+		al = null;
+	}
 
+	/**
+	 * Move Label Target Attributes. 
+	 * From Mulan Style (classes come last) to Meka Style (classes come first).
+	 */
+	public static final Instances moveAttributes(Instances D, int L) {
+		// use e.g.: java weka.filters.unsupervised.attribute.Reorder -i thyroid.arff -R 30-last,1-29"
+		return D;
+	}
 
+	public static Instances removeLabels(Instances D) {
+		int L = D.classIndex();
+		for(int i = 0; i < D.numInstances(); i++) {
+			for(int j = 0; j < L ; j++) {
+				D.instance(i).setMissing(j);
+			}
+		}
+		return D;
+	}
+
+	public static final Instances combineInstances(Instances D1, Instances D2) {
+		Instances D = new Instances(D1);
+		for(int i = 0; i < D2.numInstances(); i++) {
+			D.add(D2.instance(i));
+		}
+		return D;
+	}
+
+	public static final String toDebugString(Instances D) {
+		int L = D.classIndex();
+		StringBuilder sb = new StringBuilder();  
+		sb.append("D="+D.numInstances());
+		sb.append(" L="+L+" {");
+		for(int j = 0; j < L; j++) {
+			sb.append(D.attribute(j).name()+" ");
+		}
+		sb.append("}");
+		return sb.toString();
+	}
+	
+	public static final String toDebugString(Instance x) {
+		int L = x.classIndex();
+		StringBuilder sb = new StringBuilder();  
+		sb.append("y = [");
+		for(int j = 0; j < L; j++) {
+			sb.append(x.value(j)+" ");
+		}
+		sb.append("], x = [");
+		for(int j = L; j < L+10; j++) {
+			sb.append(x.value(j)+" ");
+		}
+		sb.append(" ... ]");
+		return sb.toString();
+	}
+
+	/*
+    public static int factorial(int n) {
+        int fact = 1; 
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+	*/
+
+	public static int[] toPrimitive(Integer a[]) {
+		int b[] = new int[a.length];
+		for(int i = 0; i < a.length; i++) {
+			b[i] = a[i];
+		}
+		return b;
+	}
+
+	private static void permute(String beginningString, String endingString, ArrayList<String> perm) {
+		if (endingString.length() <= 1) {
+			perm.add(beginningString + endingString);
+		}
+		else
+			for (int i = 0; i < endingString.length(); i++) {
+				String newString = endingString.substring(0, i) + endingString.substring(i + 1);
+				permute(beginningString + endingString.charAt(i), newString, perm);
+			}
+	}
+
+	public static String[] permute(String s) {
+		ArrayList<String> a = new ArrayList<String>();  
+		permute("", s, a);
+		return a.toArray(new String[0]);
+	}
+
+	public static String hashmapToString(HashMap<?,?> hm) {
+		StringBuilder sb = new StringBuilder();
+		for (Object k : hm.keySet()) {
+			sb.append(Utils.padLeft(k.toString(),20)).append(" : ").append(hm.get(k)).append('\n');
+		}
+		return sb.toString();
+	}
+
+	public static int getIntegerOption(String op, int def) {
+		try {
+			return Integer.parseInt(op);
+		} catch(Exception e) {
+			System.err.println("Failed to parse "+op+" to double: using default of "+def);
+			return def;
+		}
+	}
+
+	public static void main(String args[]) {
+		String p[] = permute(args[0]);
+		int i = 0;
+		for(String s: p) {
+			System.out.println(""+(i++)+" "+s);
+		}
+		//System.out.println(""+Arrays.toString(invert(new int[]{1,2},6)));
+		//System.out.println(""+Arrays.toString(invert(new int[]{0,2},6)));
+		//System.out.println(""+Arrays.toString(invert(new int[]{5,2},6)));
+	}
 
 }
