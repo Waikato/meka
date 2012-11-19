c2e5526f9056060812fe7e8768feb73a7e13d1a7
fracpete <fracpete@99739a35-adc7-4a81-af46-22eadfc39334>
2012-11-19 00:37:51 +0000

    added visualization panel updated titles

---
+++ trunk/src/main/java/meka/gui/explorer/VisualizeTab.java
@@ -0,0 +1,79 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * VisualizeTab.java
+ * Copyright (C) 2012 University of Waikato, Hamilton, New Zealand
+ */
+package meka.gui.explorer;
+
+import java.awt.BorderLayout;
+
+import weka.gui.visualize.MatrixPanel;
+
+/**
+ * For visualizing the data.
+ * 
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class VisualizeTab
+  extends AbstractExplorerTab {
+
+  /** for serialization. */
+  private static final long serialVersionUID = 7200133227901982729L;
+
+  /** the panel for visualizing the data. */
+  protected MatrixPanel m_PanelMatrix;
+  
+  /**
+   * Initializes the tab.
+   * 
+   * @param owner the Explorer this tab belongs to
+   */
+  public VisualizeTab(Explorer owner) {
+    super(owner);
+  }
+
+  /**
+   * Initializes the widgets.
+   */
+  @Override
+  protected void initGUI() {
+    super.initGUI();
+    
+    m_PanelMatrix = new MatrixPanel();
+    add(m_PanelMatrix, BorderLayout.CENTER);
+  }
+  
+  /**
+   * Returns the title of the tab.
+   * 
+   * @return the title
+   */
+  @Override
+  public String getTitle() {
+    return "Visualize";
+  }
+
+  /**
+   * Gets called when the data changed.
+   */
+  @Override
+  protected void update() {
+    if (hasData())
+      m_PanelMatrix.setInstances(getData());
+  }
+}
