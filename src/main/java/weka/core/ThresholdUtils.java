c50f44234ebd4ede3919c25c4d23c06dcfad8ea7
Jesse <jmread@users.noreply.github.com>
2013-09-01 08:24:40 +0000

    A new class for threshold utils.

---
+++ src/main/java/weka/core/ThresholdUtils.java
@@ -0,0 +1,115 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package weka.core;
+
+import weka.core.*;
+import java.util.*;
+import rbms.M;
+
+public abstract class ThresholdUtils {
+
+	/**
+	 * CalibrateThreshold - Calibrate a threshold using PCut: the threshold which results in the best approximation of the label cardinality of the training set.
+	 * @param	Y			labels
+	 * @param	LC_train	label cardinality of the training set
+	 */
+	public static double calibrateThreshold(ArrayList<double[]> Y, double LC_train) { 
+
+		if (Y.size() <= 0) 
+			return 0.5;
+
+		int N = Y.size();
+		ArrayList<Double> big = new ArrayList<Double>();
+		for(double y[] : Y) {
+			for (double y_ : y) {
+				big.add(y_);
+			}
+		}
+		Collections.sort(big);
+
+		int i = big.size() - (int)Math.round(LC_train * (double)N);
+
+		if (N == big.size()) { // special cases
+			if (i+1 == N) // only one!
+				return (big.get(N-2)+big.get(N-1)/2.0);
+			if (i+1 >= N) // zero!
+				return 1.0;
+			else
+				return Math.max(((double)(big.get(i)+big.get(i+1))/2.0), 0.00001);
+		}
+
+		return Math.max(((double)(big.get(i)+big.get(Math.max(i+1,N-1))))/2.0 , 0.00001);
+	}
+
+	/**
+	 * CalibrateThreshold - Calibrate a vector of thresholds (one for each label) using PCut: the threshold t[j] which results in the best approximation of the frequency of the j-th label in the training data.
+	 * @param	Y			labels
+	 * @param	LC_train[]	average frequency of each label
+	 */
+	public static double[] calibrateThresholds(ArrayList<double[]> Y, double LC_train[]) { 
+
+		int L = LC_train.length;
+		double t[] = new double[L];
+
+		ArrayList<double[]> Y_[] = new ArrayList[L];
+		for(int j = 0; j < L; j++) {
+			Y_[j] = new ArrayList<double[]>();
+		}
+
+		for(double y[] : Y) {
+			for(int j = 0; j < L; j++) {
+				Y_[j].add(new double[]{y[j]});
+			}
+		}
+
+		for(int j = 0; j < L; j++) {
+			t[j] = calibrateThreshold(Y_[j],LC_train[j]);
+		}
+
+		return t;
+	}
+
+	/**
+	 * Threshold - returns the labels after the prediction-confidence vector is passed through a vector of thresholds.
+	 * @param	Rpred[][]	label confidence predictions in [0,1]
+	 * @param	t[]			threshold for each label
+	 */
+	public static final int[][] threshold(double Rpred[][], double t[]) {
+		int Ypred[][] = new int[Rpred.length][Rpred[0].length];
+		for(int i = 0; i < Rpred.length; i++) {
+			for(int j = 0; j < Rpred[i].length; j++) {
+				Ypred[i][j] = (Rpred[i][j] >= t[j]) ? 1 : 0;
+			}
+		}
+		return Ypred;
+	}
+
+	/**
+	 * Threshold - returns the labels after the prediction-confidence vector is passed through threshold.
+	 * @param	Rpred[][]	label confidence predictions in [0,1]
+	 * @param	t			threshold
+	 */
+	public static final int[][] threshold(double Rpred[][], double t) {
+		int Ypred[][] = new int[Rpred.length][Rpred[0].length];
+		for(int i = 0; i < Rpred.length; i++) {
+			for(int j = 0; j < Rpred[i].length; j++) {
+				Ypred[i][j] = (Rpred[i][j] >= t) ? 1 : 0;
+			}
+		}
+		return Ypred;
+	}
+
+}
