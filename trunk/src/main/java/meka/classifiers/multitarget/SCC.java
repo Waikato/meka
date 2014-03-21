baaaebd1b779b31ee4435e22b3a7955062a23b17
jread82_nz <jread82_nz@99739a35-adc7-4a81-af46-22eadfc39334>
2014-03-21 08:03:33 +0000

    Code cleanup.

--- trunk/src/main/java/meka/classifiers/multitarget/SCC.java
+++ trunk/src/main/java/meka/classifiers/multitarget/SCC.java
@@ -101,9 +101,9 @@ public class SCC extends MultilabelClassifier implements Randomizable, MultiTarg
 		int N = D.numInstances();
 
 		// create and fill factor nodes according to 'partition'
-		Factor F[] = new Factor[partition.length];	
+		SuperClass F[] = new SuperClass[partition.length];	
 		for(int n = 0; n < partition.length; n++) {
-			F[n] = new Factor();								// create a factor node
+			F[n] = new SuperClass();								// create a factor node
 			for(int j : partition[n]) {
 				//System.out.println("add node for : "+j);
 				F[n].addNode(j,D.attribute(j).numValues());								// add indices
@@ -500,4 +500,80 @@ public class SCC extends MultilabelClassifier implements Randomizable, MultiTarg
 
 	}
 
+	class SuperClass {
+
+		SuperClass index[] = null;
+
+		int idx = -1;
+		double prob = 1.0;
+
+		public SuperClass() { 			// root
+		}
+
+		public SuperClass(int n) {		// branch
+			idx = n;
+		}
+
+		/**
+		 * AddNode - add a node with index 'j' that can take 'k' values.
+		 * @param	n	node index
+		 * @param	k	number of values
+		 */
+		public void addNode(int n, int k) {
+			double p = 0.0;
+			if (index != null) {
+				for(int v = 0; v < index.length; v++) {
+					index[v].addNode(n,k);
+				}
+			}
+			else {
+				index = new SuperClass[k];
+				for(int v = 0; v < index.length; v++) {
+					index[v] = new SuperClass(n); 		// p(x==v)
+				}
+			}
+		}
+
+		public void fillNodes(Instances D) {
+			// @todo could be much faster by prebuffering arry of size of the depth of the tree
+			fillNodes(new int[]{},new int[]{},D);
+		}
+
+		public void fillNodes(int indices[], int values[], Instances D) {
+			//System.out.println("fillNodes("+Arrays.toString(indices)+","+Arrays.toString(values)+")");
+			if (index == null) {
+				// END, calculate the joint
+				prob = StatUtils.P(D,indices,values);
+				//System.out.println("we arrived with P("+Arrays.toString(indices)+" == "+Arrays.toString(values)+") = "+prob);
+			}
+			else {
+				// GO DOWN
+				// @todo could be faster by moving the add(indices,idx) outside of the v loop (but not by much!)
+				for(int v = 0; v < index.length; v++) {
+					index[v].fillNodes(A.add(indices,index[0].idx), A.add(values,v), D);
+				}
+			}
+		}
+
+		// the probability of 'path' in this factor
+		// (only a part of the path may be relevant)
+		public double p_path(int path[]) {
+			//System.out.println(""+Arrays.toString(path));
+			//System.out.println(""+idx);
+			if (index==null) {
+				return prob;
+			}
+			else {
+				int i = index[0].idx;				// 3	\in {1,...,L}
+				int v = path[i];					// 0 	\in {0,1,..,K}
+				//System.out.println("take "+v+"th path");
+				return index[v].p_path(path);
+			}
+		}
+
+		public String toString() {
+			return (index!=null) ?  idx + "/" + index.length + "\n" + index[0].toString() : " = "+prob;
+		}
+
+	}
 }
