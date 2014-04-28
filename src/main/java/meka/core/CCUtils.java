1fc82f31bc6ae0b596a25a72842bf6dbc1bb35ab
Jesse <jmread@users.noreply.github.com>
2014-04-28 11:41:47 +0000

    Reorganisation and update of the CC-family code.

--- src/main/java/meka/core/CCUtils.java
+++ src/main/java/meka/core/CCUtils.java
@@ -15,10 +15,74 @@
 
 package meka.core;
 
+import meka.classifiers.multilabel.CCe;
+import weka.classifiers.Classifier;
 import weka.core.*;
 import java.util.*;
 
 public abstract class CCUtils {
+
+	/* 
+	 * Build h_{s} : X -> Y
+	 */
+	public static CCe buildCC(int chain[], Instances D, Classifier g) throws Exception {
+
+		// a new classifier chain
+		CCe h = new CCe();
+
+		// build this chain
+		h.setChain(chain);
+		h.setClassifier(g);
+		h.buildClassifier(new Instances(D));
+		return h;
+	}
+
+	/*
+			 * Use this code to try ALL possible combinations
+			 *
+	public static int[] ChainSearchOptimal(Instances D_train, Instances D_test, Classifier c) {
+		String perms[] = MLUtils.permute(MLUtils.toBitString(MLUtils.gen_indices(L)));
+		for (int t = 0; t < perms.length; t++) {
+			int s_[] = A.string2IntArray(perms[t]);
+			System.out.println("proposing s' = perm["+t+"] = "+Arrays.toString(s_));
+		}
+	}
+	*/
+
+	/**
+	 * RandomSearch. Basically Simulated Annealing without temperature. Start searching from y0[]
+	 */
+	public static double[] RandomSearch(CCe h, Instance x, int T, Random r, double y0[]) throws Exception {
+
+		double y[] = Arrays.copyOf(y0,y0.length); 				// prior y
+		double w  = A.product(h.probabilityForInstance(x,y));	// p(y|x)
+		//System.out.println("y0="+Arrays.toString(y)+"\tw0="+w);
+
+		//if (getDebug()) System.out.println(" y0 = "+MLUtils.toBitString(y) +" w0 = "+w);
+		y = Arrays.copyOf(y,y.length);
+		for(int t = 0; t < T; t++) {
+			// propose y' by sampling i.i.d.
+			double y_[] = h.sampleForInstance(x,r); 	       
+			// rate y' as w'
+			double w_  = A.product(h.getConfidences());
+			//System.out.println("y_="+Arrays.toString(y_)+"\tw_="+w_);
+			// accept ? 
+			if (w_ > w) {
+				w = w_;
+				y = y_;
+				//if (getDebug()) System.out.println(" y' = "+MLUtils.toBitString(y_) +" w' = "+w_+ " (accept!)"); 
+			}
+		}
+		return y;
+	}
+
+	/**
+	 * RandomSearch. Basically Simulated Annealing without temperature.
+	 */
+	public static double[] RandomSearch(CCe h, Instance x, int T, Random r) throws Exception {
+
+		return RandomSearch(h,x,T,r,h.distributionForInstance(x));
+	}
 
 	/**
 	 * SetPath - set 'path[]' into the first L attributos of Instance 'xy'.
