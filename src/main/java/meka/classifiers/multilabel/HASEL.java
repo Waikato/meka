a21690a5eeca19333481c6633cd5fa8cf09b6b0d
Jesse <jmread@users.noreply.github.com>
2014-10-14 06:25:35 +0000

    Added HASEL: HierarchicAlly defined Sets of labELs

---
+++ src/main/java/meka/classifiers/multilabel/HASEL.java
@@ -0,0 +1,79 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package meka.classifiers.multilabel;
+
+import weka.classifiers.*;
+import weka.classifiers.meta.*;
+import meka.classifiers.multilabel.*;
+import weka.core.*;
+import meka.core.*;
+import weka.filters.unsupervised.attribute.*;
+import weka.filters.*;
+import java.util.*;
+
+/**
+ * HASEL - Partitions labels into subsets based on the dataset defined hierarchy (assuming that a '.' in the attribute names defines hierarchical branches, e.g., "Europe.Spain").
+ * @author 	Jesse Read 
+ * @version June 2014
+ */
+public class HASEL extends RAkELd {
+
+	/** for serialization. */
+	private static final long serialVersionUID = -6208388889440497988L;
+
+	/**
+	 * Description to display in the GUI.
+	 * 
+	 * @return		the description
+	 */
+	@Override
+	public String globalInfo() {
+		return "Partitions labels into subsets based on the dataset defined hierarchy (assuming that a '.' in the attribute names defines hierarchical branches, e.g., \"Europe.Spain\").";
+	}
+
+	@Override
+	public void buildClassifier(Instances D) throws Exception {
+
+		int L = D.classIndex();
+		int N = D.numInstances();
+
+		// Get partition from dataset hierarchy
+		kMap = SuperLabelUtils.getPartitionFromDatasetHierarchy(D); 
+		m_M = kMap.length;
+		m_Classifiers = AbstractClassifier.makeCopies(m_Classifier,m_M);
+		m_InstancesTemplates = new Instances[m_M];
+
+		for(int i = 0; i < m_M; i++) {
+
+			if (getDebug()) 
+				System.out.println("Building model "+(i+1)+"/"+m_M+": "+Arrays.toString(kMap[i]));
+			Instances D_i = SuperLabelUtils.makePartitionDataset(D,kMap[i]);
+			m_Classifiers[i].buildClassifier(D_i);
+			m_InstancesTemplates[i] = new Instances(D_i,0);
+		}
+
+	}
+
+	@Override
+	public String getRevision() {
+	    return RevisionUtils.extract("$Revision: 9117 $");
+	}
+
+	public static void main(String args[]) {
+		MultilabelClassifier.evaluation(new HASEL(),args);
+	}
+
+}
