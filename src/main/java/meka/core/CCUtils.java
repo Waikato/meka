ca6d44750cbc6ab688d693df83cb2337ed51a0b1
Jesse <jmread@users.noreply.github.com>
2015-08-26 11:51:21 +0000

    Minor code cleanup, deleted old comments.

--- src/main/java/meka/core/CCUtils.java
+++ src/main/java/meka/core/CCUtils.java
@@ -42,80 +42,49 @@ public abstract class CCUtils {
 		return h;
 	}
 
-	/*
-			 * Use this code to try ALL possible combinations
-			 *
-	public static int[] ChainSearchOptimal(Instances D_train, Instances D_test, Classifier c) {
-		String perms[] = MLUtils.permute(MLUtils.toBitString(MLUtils.gen_indices(L)));
-		for (int t = 0; t < perms.length; t++) {
-			int s_[] = A.string2IntArray(perms[t]);
-			System.out.println("proposing s' = perm["+t+"] = "+Arrays.toString(s_));
-		}
+	/* 
+	 * BuildCL - Given a base classifier 'g', build a new CL classifier on data D, given chain order 'chain'.
+	public static CL buildCL(int chain[], Instances D, Classifier g) throws Exception {
+
+		// a new classifier chain
+		CL h = new CL();
+
+		// build this chain
+		h.setChain(chain);
+		h.setClassifier(g);
+		h.buildClassifier(new Instances(D));
+		return h;
 	}
 	*/
 
-	/*
-	 * RandomSearch -  Basically Simulated Annealing without temperature, starting from y0[].
-	public static double[] RandomSearch(CC h, Instance x, int T, Random r, double y0[]) throws Exception {
+	/** 
+	 * BuildCT - Given a base classifier 'g', build a new CT classifier on data D, given chain order 'chain'.
+	 * TODO: combine with above function
+	 */
+	public static CT buildCT(int chain[], Instances D, Classifier g) throws Exception {
 
-		double y[] = Arrays.copyOf(y0,y0.length); 				// prior y
-		double w  = A.product(h.probabilityForInstance(x,y));	// p(y|x)
+		// a new classifier chain
+		CT h = new CT();
+		h.setChainIterations(0);
 
-		Instance t_[] = h.getTransformTemplates(x);
-
-		//System.out.println("----");
-		//System.out.println("p0("+Arrays.toString(y)+") = "+Arrays.toString(h.getConfidences())+", w="+w);
-		for(int t = 0; t < T; t++) {
-			double y_[] = h.sampleForInstanceFast(t_,r); 	    // propose y' by sampling i.i.d.
-			double p_[] = h.getConfidences();                   //
-			double w_  = A.product(h.getConfidences()); 		// rate y' as w'  --- @TODO allow for command-line option
-			//System.out.println("p("+Arrays.toString(y_)+") = "+Arrays.toString(p_)+", w="+w_);
-			if (w_ > w) { 										// accept ? 
-				//if (getDebug()) System.out.println("y' = "+Arrays.toString(y_)+", :"+w_);
-				w = w_;
-				y = y_;
-				//System.out.println("* ACCEPT *");
-			}
-		}
-		return y;
+		// build this chain
+		h.setChain(chain);
+		h.setClassifier(g);
+		h.buildClassifier(new Instances(D));
+		return h;
 	}
-	*/
 
 	/*
-	// An old slower version of the above
-	@Deprecated
-	public static double[] RandomSearchOLD(CC h, Instance x, int T, Random r, double y0[]) throws Exception {
-
-		double y[] = Arrays.copyOf(y0,y0.length); 				// prior y
-		double w  = A.product(h.probabilityForInstance(x,y));	// p(y|x)
-		//System.out.println("y0="+Arrays.toString(y)+"\tw0="+w);
-
-		//if (getDebug()) System.out.println(" y0 = "+MLUtils.toBitString(y) +" w0 = "+w);
-		y = Arrays.copyOf(y,y.length);
-		for(int t = 0; t < T; t++) {
-			// propose y' by sampling i.i.d.
-			double y_[] = h.sampleForInstance(x,r); 	       
-			// rate y' as w'
-			double w_  = A.product(h.getConfidences());
-			//System.out.println("y_="+Arrays.toString(y_)+"\tw_="+w_);
-			// accept ? 
-			if (w_ > w) {
-				w = w_;
-				y = y_;
-				//if (getDebug()) System.out.println(" y' = "+MLUtils.toBitString(y_) +" w' = "+w_+ " (accept!)"); 
-			}
-		}
-		return y;
-	}
-	*/
-
-	/*
-	 * RandomSearch - Basically Simulated Annealing without temperature.
-	public static double[] RandomSearch(CC h, Instance x, int T, Random r) throws Exception {
-
-		return RandomSearch(h,x,T,r,h.distributionForInstance(x));
-	}
-	*/
+	 * Use this code to try ALL possible combinations
+	 *
+	 public static int[] ChainSearchOptimal(Instances D_train, Instances D_test, Classifier c) {
+	 String perms[] = MLUtils.permute(MLUtils.toBitString(MLUtils.gen_indices(L)));
+	 for (int t = 0; t < perms.length; t++) {
+	 int s_[] = A.string2IntArray(perms[t]);
+	 System.out.println("proposing s' = perm["+t+"] = "+Arrays.toString(s_));
+	 }
+	 }
+	 */
 
 	/**
 	 * SetPath - set 'path[]' into the first L attributes of Instance 'xy'.
