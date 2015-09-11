116e8e1e4a52129eb9412764017aa1e868466456
fracpete <fracpete@gmail.com>
2015-09-11 23:08:11 +0000

    modified interface slightly

--- src/main/java/meka/gui/explorer/classify/ShowGraphs.java
+++ src/main/java/meka/gui/explorer/classify/ShowGraphs.java
@@ -68,13 +68,13 @@ public class ShowGraphs
 	/**
 	 * Checks whether the current item can be handled. Disables/enables the menu item.
 	 *
-	 * @param suffix    the suffix of the item
-	 * @param item      the item itself
+	 * @param history   the current history
+	 * @param index     the selected history item
 	 * @return          true if can be handled
 	 */
 	@Override
-	public boolean handles(String suffix, Object item) {
-		return (item instanceof MultiLabelDrawable);
+	public boolean handles(ResultHistoryList history, int index) {
+		return (history.getPayloadAt(index) instanceof MultiLabelDrawable);
 	}
 
 	/**
