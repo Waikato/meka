0a9bc40367763f134764f410cac0291a294eb3e1
Jesse Read <jesse.read@gmail.com>
2016-01-05 14:38:02 +0200

    cleared pages branch

--- src/main/scripts/run.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-
-SET MEMORY=512m
-SET MAIN=meka.gui.guichooser.GUIChooser
-
-java -Xmx%MEMORY% -cp ".\lib\*" %MAIN% %1
--- src/main/scripts/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-MEMORY=512m
-MAIN=meka.gui.guichooser.GUIChooser
-
-java -Xmx$MEMORY -cp "./lib/*" $MAIN $1
-
