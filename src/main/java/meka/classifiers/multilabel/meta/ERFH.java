fe50649c566b94a4734ad2a839b3ee86870f3a48
Aaron Keesing <agkphysics@gmail.com>
2018-10-03 02:21:55 +1300

    Change bagging to duplication of instances. Added option for meta-classifier and single label classifier.

--- src/main/java/meka/classifiers/multilabel/meta/ERFH.java
+++ src/main/java/meka/classifiers/multilabel/meta/ERFH.java
@@ -8,6 +8,7 @@ import java.util.*;
 import meka.classifiers.multilabel.*;
 import meka.classifiers.multilabel.meta.HOMER.ClusterLabelSplitter;
 import meka.core.*;
+import weka.classifiers.SingleClassifierEnhancer;
 import weka.classifiers.trees.RandomForest;
 import weka.core.*;
 import weka.core.TechnicalInformation.Field;
@@ -18,8 +19,12 @@ import weka.core.TechnicalInformation.Type;
  * 
  * @author Aaron Keesing
  */
-public class ERFH extends BaggingML implements Randomizable, TechnicalInformationHandler {
+public class ERFH extends MetaProblemTransformationMethod implements Randomizable, TechnicalInformationHandler {
 
+    /**
+     * The same as {@link ClusterLabelSplitter} except uses a random number of
+     * splits at each node of the tree, within fixed bounds.
+     */
     public static class VariableKLabelSplitter extends ClusterLabelSplitter {
         private static final long serialVersionUID = 9211371179003763478L;
         private Random r;
@@ -37,10 +42,13 @@ public class ERFH extends BaggingML implements Randomizable, TechnicalInformatio
 
     private static final long serialVersionUID = 5482843422132209885L;
 
+    private SingleClassifierEnhancer metaClassifier;
+
     protected double threshold = 0.4;
+    protected String metaClassifierString = "BinaryRelevance";
 
     public ERFH() {
-        m_Classifier = new HOMER();
+        m_Classifier = new RandomForest();
     }
 
     public double getThreshold() {
@@ -55,44 +63,64 @@ public class ERFH extends BaggingML implements Randomizable, TechnicalInformatio
         return "Prediction threshold for the multi-label classifier distribution.";
     }
 
+    public void setMetaClassifier(SingleClassifierEnhancer classifier) {
+        metaClassifier = classifier;
+    }
+
+    public String getMetaClassifierString() {
+        return metaClassifierString;
+    }
+
+    public void setMetaClassifierString(String str) {
+        if (str.equals("ClassifierChains")) {
+            metaClassifier = new CC();
+        } else {
+            metaClassifier = new BR();
+        }
+    }
+
+    public String metaClassifierStringTipText() {
+        return "Meta-classifier for each HOMER node. One of {BinaryRelevance, ClassifierChains}";
+    }
+
     /* (non-Javadoc)
      * @see meka.classifiers.multilabel.AbstractMultiLabelClassifier#buildClassifier(weka.core.Instances)
      */
     @Override
     public void buildClassifier(Instances D) throws Exception {
         testCapabilities(D);
+        int numInstances = D.numInstances();
 
         if (getDebug())
-            System.out.println("Building " + m_NumIterations + " HOMER trees.");
-        m_Classifiers = ProblemTransformationMethod.makeCopies((MultiLabelClassifier)m_Classifier, m_NumIterations);
-
+            System.out.print("Building " + m_NumIterations + " HOMER trees:");
+        m_Classifiers = new HOMER[m_NumIterations];
         for(int i = 0; i < m_NumIterations; i++) {
             Random r = new Random(m_Seed + i);
-            Instances bag = new Instances(D);
+            Instances bag = new Instances(D, 0);    
+            m_Classifiers[i] = new HOMER();
+            ((HOMER)m_Classifiers[i]).setClassifier(ProblemTransformationMethod.makeCopy(metaClassifier));
             ((HOMER)m_Classifiers[i]).setSeed(m_Seed + i);
-            if(getDebug())
-                System.out.print("" + i + " ");
+            ((HOMER)m_Classifiers[i]).setLabelSplitter(new VariableKLabelSplitter(m_Seed + i));
+            if (getDebug())
+                System.out.print(" " + i);
 
-            int ixs[] = new int[D.numInstances()];
-            for(int j = 0; j < ixs.length; j++)
-                ixs[r.nextInt(ixs.length)]++;
-            for(int j = 0; j < ixs.length; j++) {
-                if (ixs[j] > 0) {
-                    Instance instance = D.instance(j);
-                    instance.setWeight(ixs[j]);
-                    bag.add(instance);
-                }
+            for (int j = 0; j < numInstances; j++)
+                bag.add(D.get(r.nextInt(numInstances)));
+
+            if (m_Classifier instanceof RandomForest) {
+                RandomForest rf = (RandomForest)m_Classifier;
+                rf.setSeed(m_Seed + i);
+                rf.setMaxDepth(r.nextInt(5) + 1);
+                rf.setBagSizePercent(r.nextInt(21) + 60);
+                rf.setNumIterations(r.nextInt(3) + 1);
             }
-
-            ((HOMER)m_Classifiers[i]).setLabelSplitter(new VariableKLabelSplitter(m_Seed));
-            RandomForest rf = new RandomForest();
-            rf.setSeed(m_Seed + i);
-            rf.setMaxDepth(r.nextInt(5) + 1);
-            rf.setBagSizePercent(r.nextInt(21) + 60);
-            rf.setNumIterations(r.nextInt(3) + 1);
-            ((BR)((HOMER)m_Classifiers[i]).getClassifier()).setClassifier(rf);
+            ((SingleClassifierEnhancer)((HOMER)m_Classifiers[i]).getClassifier()).setClassifier(m_Classifier);
+            ((Randomizable)((HOMER)m_Classifiers[i]).getClassifier()).setSeed(m_Seed + i);
             m_Classifiers[i].buildClassifier(bag);
         }
+        
+        if (getDebug())
+            System.out.println();
     }
 
     @Override
@@ -107,6 +135,7 @@ public class ERFH extends BaggingML implements Randomizable, TechnicalInformatio
     public String[] getOptions() {
         List<String> result = new ArrayList<>();
         OptionUtils.add(result, "t", threshold);
+        OptionUtils.add(result, "M", metaClassifierString);
         OptionUtils.add(result, super.getOptions());
         return OptionUtils.toArray(result);
     }
@@ -115,14 +144,21 @@ public class ERFH extends BaggingML implements Randomizable, TechnicalInformatio
     public void setOptions(String[] options) throws Exception {
         super.setOptions(options);
         threshold = OptionUtils.parse(options, "t", 0.4);
+        setMetaClassifierString(OptionUtils.parse(options, "M", ""));
     }
 
     @Override
     public Enumeration<Option> listOptions() {
         Vector<Option> options = new Vector<>();
         options.add(new Option(thresholdTipText(), "threshold", 1, "-t threshold"));
+        options.add(new Option(metaClassifierStringTipText(), "metaClassifier", 1, "-M string"));
         OptionUtils.add(options, super.listOptions());
         return options.elements();
+    }
+
+    @Override
+    public String globalInfo() {
+        return "Extremely Randomised Forest of HOMER trees.";
     }
 
     @Override
